a0119fb98c45bd150a32a0a06034c3ea4cd2fd29
shashi betadur <shashi.betadur@gmail.com>
2011-11-05 20:25:37 +0530

    [Shashi] - deploy.bat

--- code/deploy.bat
+++ code/deploy.bat
@@ -1,3 +1,3 @@
-RMDIR /s /q F:\apache-tomcat-6.0.33\webapps\bis
-RMDIR /s /q F:\apache-tomcat-6.0.33\work\Catalina\localhost\bis
-xcopy /s /e web\target\web-1.0 F:\apache-tomcat-6.0.33\webapps\bis\
+RMDIR /s /q %TOMCAT_HOME%\webapps\bis
+RMDIR /s /q %TOMCAT_HOME%\work\Catalina\localhost\bis
+xcopy /s /e web\target\web-1.0 %TOMCAT_HOME%\webapps\bis\

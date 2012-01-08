e4c4bd0a4fa178f8b1b1892f9582e4a7974d5e4f
unknown <praveends@PRAVEEN-PC.(none)>
2012-01-08 14:22:44 +0530

    [Praveen] Moved release 1 scripts to seperate folder

---
+++ database/Release_1.0/drop_tables.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS Item_Vendor;
+
+DROP TABLE IF EXISTS PT_Details;
+
+DROP TABLE IF EXISTS ST_Details;
+
+DROP TABLE IF EXISTS Payment_History_Procurement;
+
+DROP TABLE IF EXISTS Billing_Procurement;
+
+DROP TABLE IF EXISTS Billing_Sales;
+
+DROP TABLE IF EXISTS Sales_Transaction;
+
+DROP TABLE IF EXISTS Stock;
+
+DROP TABLE IF EXISTS Payment_History_Sales;
+
+DROP TABLE IF EXISTS Procurement_Transaction;
+
+DROP TABLE IF EXISTS Vendor;
+
+DROP TABLE IF EXISTS Item;
+
+DROP TABLE IF EXISTS Hawker;
+
+
